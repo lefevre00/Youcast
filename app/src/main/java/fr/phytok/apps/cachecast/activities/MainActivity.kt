115915847586082866ccf521a943c3c9315593b4
Michaël Lefèvre <michael.lefevre@itk.fr>
2021-12-23 18:51:07 +0100

    Check permission on start, and store media file

--- app/src/main/java/fr/phytok/apps/cachecast/activities/MainActivity.kt
+++ app/src/main/java/fr/phytok/apps/cachecast/activities/MainActivity.kt
@@ -1,18 +1,73 @@
 package fr.phytok.apps.cachecast.activities
 
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 import fr.phytok.apps.cachecast.BuildConfig
 import fr.phytok.apps.cachecast.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<TextView>(R.id.serverValue).setText(BuildConfig.SERVER)
+
+        requestPermission()
+    }
+    /**
+     * Convenience method to check if [Manifest.permission.READ_EXTERNAL_STORAGE] permission
+     * has been granted to the app.
+     */
+    private fun haveStoragePermission() =
+        ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PERMISSION_GRANTED
+
+    /**
+     * Convenience method to request [Manifest.permission.READ_EXTERNAL_STORAGE] permission.
+     */
+    private fun requestPermission() {
+        if (!haveStoragePermission()) {
+            val permissions = arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            ActivityCompat.requestPermissions(this, permissions,
+                READ_EXTERNAL_STORAGE_REQUEST
+            )
+        }
     }
 
-    // TODO load list of audio file download
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            READ_EXTERNAL_STORAGE_REQUEST -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PERMISSION_GRANTED) {
+                    Log.d(TAG, "Permission granted")
+                } else {
+                    Log.d(TAG, "Permission refused")
+                }
+            }
+        }
+    }
+
+    companion object {
+        /** The request code for requesting [Manifest.permission.READ_EXTERNAL_STORAGE] permission. */
+        private const val READ_EXTERNAL_STORAGE_REQUEST = 0x1045
+
+        val TAG = "MainActivity"
+    }
+
 }
--- app/src/main/java/fr/phytok/apps/cachecast/yas/RemoteTrackRepository.kt
+++ app/src/main/java/fr/phytok/apps/cachecast/yas/RemoteTrackRepository.kt
@@ -1,6 +1,11 @@
 package fr.phytok.apps.cachecast.yas
 
+import android.content.ContentValues
 import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import dagger.hilt.android.qualifiers.ApplicationContext
 import okhttp3.ResponseBody
@@ -8,7 +13,6 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.io.*
-import java.nio.file.Paths
 import javax.inject.Inject
 
 class RemoteTrackRepository @Inject constructor(
@@ -34,14 +38,65 @@ class RemoteTrackRepository @Inject constructor(
     }
 
     fun downloadTrack(video: String) {
-        yasClient.trackById(video).execute().body()?.let { saveToDisk(it, video) }
+//        yasClient.trackById(video).execute().body()?.let { saveToExternalStorage(it, video) }
+        yasClient.trackById(video).execute().body()?.let { saveToMediaStore(it, video) }
     }
 
-    private fun saveToDisk(body: ResponseBody, video: String) {
+    private fun saveToMediaStore(body: ResponseBody, video: String) {
+        // Add a media item that other apps shouldn't see until the item is
+// fully written to the media store.
+        val resolver = context.contentResolver
+
+// Find all audio files on the primary external storage device.
+        val audioCollection =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                MediaStore.Audio.Media.getContentUri(
+                    MediaStore.VOLUME_EXTERNAL_PRIMARY
+                )
+            } else {
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+            }
+
+        val songDetails = ContentValues().apply {
+            val title = "'$video.mp3'"
+            put(MediaStore.Audio.Media.DISPLAY_NAME, title)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.Audio.Media.IS_PENDING, 1)
+            }
+
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+                val directory = context.getExternalFilesDir(Environment.DIRECTORY_MUSIC)
+                put(MediaStore.Audio.Media.DATA, "${directory}$title")
+            }
+        }
+
+        var trackUri: Uri? = null
+        resolver.insert(audioCollection, songDetails)?.let { songContentUri ->
+            trackUri = songContentUri
+            resolver.openFileDescriptor(songContentUri, "w", null)?.use { pfd ->
+                FileOutputStream(pfd.fileDescriptor).use { outputStream ->
+                    body.byteStream().use { inputStream ->
+                        copyContent(inputStream, outputStream)
+                    }
+                }
+            }
+            Log.d(TAG, "Track saved as sound media ")
+        } ?: Log.e(TAG, "Failed to saved track")
+
+        // Update status if possible
+        trackUri?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                songDetails.clear()
+                songDetails.put(MediaStore.Audio.Media.IS_PENDING, 0)
+                resolver.update(trackUri!!, songDetails, null, null)
+                Log.d(TAG, "Track marked as ready to use")
+            }
+        }
+    }
+
+    private fun saveToExternalStorage(body: ResponseBody, video: String) {
+        // TODO store as soon as the stream begin
         Log.d(TAG, "MP3 track received")
-//        Log.d(TAG, "MP3 response length ${body.contentLength()}")
-//        Log.d(TAG, "MP3 response type ${body.contentType()}")
-//        Log.d(TAG, "MP3 response exhausted ${body.source().exhausted()}")
 
         val cacheDir = context.getExternalFilesDir(null)
         val filePath = arrayOf(cacheDir, "$video.mp3").joinToString(File.separator)
@@ -55,23 +110,27 @@ class RemoteTrackRepository @Inject constructor(
         try {
             body.byteStream().use { inputStream ->
                 FileOutputStream(filePath).use { outputStream ->
-                    val buffer = ByteArray(4096)
-                    var fileSizeDownloaded: Long = 0
-                    while (true) {
-                        val read: Int = inputStream.read(buffer)
-                        if (read == -1) {
-                            break
-                        }
-                        outputStream.write(buffer, 0, read)
-                        fileSizeDownloaded += read.toLong()
-                        Log.d("File download: ", "$fileSizeDownloaded")
-                    }
-                    outputStream.flush()
+                    copyContent(inputStream, outputStream)
                 }
             }
         } catch (e: IOException) {
             Log.w(TAG, "Should handle exception", e)
         }
+    }
+
+    private fun copyContent(inputStream: InputStream, outputStream: FileOutputStream) {
+        val buffer = ByteArray(4096)
+        var fileSizeDownloaded: Long = 0
+        while (true) {
+            val read: Int = inputStream.read(buffer)
+            if (read == -1) {
+                break
+            }
+            outputStream.write(buffer, 0, read)
+            fileSizeDownloaded += read.toLong()
+            Log.d("File download: ", "$fileSizeDownloaded")
+        }
+        outputStream.flush()
     }
 
     companion object {
